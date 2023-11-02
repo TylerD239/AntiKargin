e7f10d375d8c845cc3781a26ab8f6f256b18a4b4
Kirill Smolin <kirill.smolin@kosmos.games>
2023-11-02 13:31:12 +0300

    fix column name

--- createDB.js
+++ createDB.js
@@ -4,7 +4,7 @@ const db = new sqlite3.Database('db');
 const initialWords = ['купат', 'кyпат', 'куп@т', 'kупат', 'kyпат', 'полиров', 'бутыл'];
 
 db.serialize(() => {
-    db.run("CREATE TABLE words (words TEXT)");
+    db.run("CREATE TABLE words (word TEXT)");
     db.run(`INSERT INTO words(word) VALUES ${initialWords.map(_=>('(?)'))}`,
         initialWords,
         function(error){
