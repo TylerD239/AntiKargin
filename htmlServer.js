b5119dfbe44ca492219d98454ae66af3f8669efd
Kirill Smolin <kirill.smolin@kosmos.games>
2023-11-17 21:41:44 +0300

    html

--- htmlServer.js
+++ htmlServer.js
@@ -1,10 +1,24 @@
-const app = require('express')()
+const express = require('express');
+const app = express();
 const port = 80;
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('db');
 
 app.get('/', function (req, res) {
-    res.send('Здарова ебать');
+    res.sendFile(__dirname + "/index.html");
 })
 
 app.listen(port, () => {
     console.log(`Ну, слушаю ${port}`);
 });
+
+app.get('/test', async (req, res) => {
+    db.all(`SELECT * FROM deletedMessages`, (error, rows) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        res.send(rows);
+    });
+});
+
