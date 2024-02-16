ad10411e49ac8e725f3cfdc9846b5323a3cd046d
Kirill Smolin <kirill.smolin@kosmos.games>
2024-02-16 12:13:38 +0300

    log2

--- htmlServer.js
+++ htmlServer.js
@@ -5,7 +5,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('db');
 
 app.get('/', function (req, res) {
-    console.log(req.referrer);
+    console.log(req.url);
     res.sendFile(__dirname + "/index.html");
 })
 
