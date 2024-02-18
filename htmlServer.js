fa7c42714386f9683720280f92e40ccda87e78f1
Kirill Smolin <kirill.smolin@kosmos.games>
2024-02-18 14:20:36 +0300

    abobus

--- htmlServer.js
+++ htmlServer.js
@@ -4,8 +4,12 @@ const port = 80;
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('db');
 
+// app.use(express.static('public'))
 app.get('/', function (req, res) {
-    console.log(req.hostname);
+    if (req.hostname === "vsratan.ru") {
+        res.sendFile(__dirname + "/public/img.jpg");
+        return;
+    }
     res.sendFile(__dirname + "/index.html");
 })
 
